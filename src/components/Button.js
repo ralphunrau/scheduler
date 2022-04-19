db081b8a797d0f1c174a681a1fa8d2bdbcf4a756
Ralph Unrau <xralphunrau@gmail.com>
2022-04-19 15:22:51 -0700

    Add styles to dayListItems when full or selected

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "components/Button.scss";
 import classNames from "classnames";
+import "components/Button.scss";
 
 export default function Button(props) {
-   
+
    const buttonClass = classNames('button', {
       ' button--confirm': props.confirm,
       ' button--danger': props.danger
---
+++ src/components/DayList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classNames from "classnames";
+import DayListItem from "components/DayListItem";
+
+function DayList(props) {
+  return (
+
+  );
+}
